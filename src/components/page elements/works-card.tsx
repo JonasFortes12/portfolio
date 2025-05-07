d9ce51d861fdc1d9876d30f1f58233f0971f9120
JonasFortes12 <jonascforte@hotmail.com>
2025-05-07 20:35:33 -0300

    refact: create component work-card

---
+++ src/components/page elements/works-card.tsx
@@ -0,0 +1,43 @@
+import { Card, CardHeader, CardBody, Image, Button } from "@heroui/react";
+
+interface WorksCardProps {
+    title: string;
+    description: string;
+    imageUrl: string;
+    link: string;
+}
+
+export default function WorksCard(props: WorksCardProps) {
+
+    return (
+        <Card className="py-4">
+          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+            <h4 className="font-bold text-large">{props.title}</h4>
+            <small className="text-default-500 h-16">
+              {props.description}
+            </small>
+          </CardHeader>
+          <CardBody className="overflow-visible py-2 flex-col items-center gap-3">
+            <Image
+              alt="Card background"
+              className="object-cover rounded-xl w-full"
+              src={props.imageUrl}
+            />
+            <Button
+              className="text-tiny w-2/3"
+              color="primary"
+              radius="full"
+              size="sm"
+              onPress={() =>
+                window.open(
+                    props.link,
+                  "_blank"
+                )
+              }
+            >
+              Saiba mais
+            </Button>
+          </CardBody>
+        </Card>
+    )
+}
--- src/components/page elements/works.tsx
+++ src/components/page elements/works.tsx
@@ -1,5 +1,6 @@
 import SectionTitle from "@/components/page elements/section-title";
 import { Card, CardHeader, CardBody, Image, Button } from "@heroui/react";
+import WorksCard from "@/components/page elements/works-card";
 
 export default function Works() {
   return (
@@ -13,125 +14,41 @@ export default function Works() {
         sm:grid-cols-1 sm:gap-y-5 sm:px-4
         "
       >
-        <Card className="py-4">
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <h4 className="font-bold text-large">Mandacaru Broker</h4>
-            <small className="text-default-500 h-16">
-              O projeto Mandacaru Broker API é uma aplicação Spring Boot que
-              visa simular o comportamento de um Home Broker de Ações.
-            </small>
-          </CardHeader>
-          <CardBody className="overflow-visible py-2 flex-col items-center gap-3">
-            <Image
-              alt="Card background"
-              className="object-cover rounded-xl w-full"
-              src="/images/mandacaru.gif"
-            />
-            <Button
-              className="text-tiny w-2/3"
-              color="primary"
-              radius="full"
-              size="sm"
-              onPress={() =>
-                window.open(
-                  "https://www.linkedin.com/feed/update/urn:li:activity:7172356161499860992/",
-                  "_blank"
-                )
-              }
-            >
-              Saiba mais
-            </Button>
-          </CardBody>
-        </Card>
+        <WorksCard
+          title="Mandacaru Broker"
+          description="O projeto Mandacaru Broker API é 
+          uma aplicação Spring Boot que visa simular o comportamento 
+          de um Home Broker de Ações."
+          imageUrl="/images/mandacaru.gif"
+          link="https://www.linkedin.com/feed/update/urn:li:activity:7172356161499860992/"
+        />
 
-        <Card className="py-4">
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <h4 className="font-bold text-large">Meicash</h4>
-            <small className="text-default-500 h-16">
-              O MEICash é um sistema de apoio à organização financeira de Micro
-              Empreendedores Individuais (MEIs).
-            </small>
-          </CardHeader>
-          <CardBody className="overflow-visible py-2 flex-col items-center gap-3">
-            <Image
-              alt="Card background"
-              className="object-cover rounded-xl w-full"
-              src="/images/meicash.gif"
-            />
-            <Button
-              className="text-tiny w-2/3"
-              color="primary"
-              radius="full"
-              size="sm"
-              onPress={() =>
-                window.open(
-                  "https://www.linkedin.com/feed/update/urn:li:activity:7209760252248944640/",
-                  "_blank"
-                )
-              }
-            >
-              Saiba mais
-            </Button>
-          </CardBody>
-        </Card>
+        <WorksCard
+          title="Meicash"
+          description="O MEICash é um sistema de apoio à organização 
+          financeira de Micro
+            Empreendedores Individuais (MEIs)."
+          imageUrl="/images/meicash.gif"
+          link="https://www.linkedin.com/feed/update/urn:li:activity:7209760252248944640/"
+        />
 
-        <Card className="py-4">
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <h4 className="font-bold text-large">Solar System Simulation</h4>
-            <small className="text-default-500 h-16">
-            Fruto de um profundo estudo em computação gráfica, 
+        <WorksCard
+          title="Solar System Simulation"
+          description=" Fruto de um profundo estudo em computação gráfica, 
             o projeto simula o sistema solar com planetas, 
-            luas, cinturão de asteroides e navegação por câmera.
-            </small>
-          </CardHeader>
-          <CardBody className="overflow-visible py-2 flex-col items-center gap-3">
-            <Image
-              alt="Card background"
-              className="object-cover rounded-xl w-full"
-              src="/images/gif-solar-system.gif"
-            />
-            <Button
-              className="text-tiny w-2/3"
-              color="primary"
-              radius="full"
-              size="sm"
-              onPress={() => window.open(
-                "https://github.com/JonasFortes12/Solar-System-Simulation", "_blank"
-              )}
-            >
-              Saiba mais
-            </Button>
-          </CardBody>
-        </Card>
+            luas, cinturão de asteroides e navegação por câmera."
+          imageUrl="/images/gif-solar-system.gif"
+          link="https://github.com/JonasFortes12/Solar-System-Simulation"
+        />
 
-        <Card className="py-4">
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <h4 className="font-bold text-large">Mapa Sonoro</h4>
-            <small className="text-default-500 h-16">
-              Mapa sonoro interativo que explora a diversidade sonora de
+        <WorksCard
+          title="Mapa Sonoro"
+          description="Mapa sonoro interativo que explora a diversidade sonora de
               Picos-PI nos anos 90, permitindo ao usuário ouvir sons de
-              diferentes locais da cidade.
-            </small>
-          </CardHeader>
-          <CardBody className="overflow-visible py-2 flex-col items-center gap-3">
-            <Image
-              alt="Card background"
-              className="object-cover rounded-xl w-full"
-              src="/images/mapa-sonoro.gif"
-            />
-            <Button
-              className="text-tiny w-2/3"
-              color="primary"
-              radius="full"
-              size="sm"
-              onPress={() => window.open(
-                "https://mapa-sonoro-de-picos-git-main-sufato1287gmailcoms-projects.vercel.app/", "_blank"
-              )}
-            >
-              Saiba mais
-            </Button>
-          </CardBody>
-        </Card>
+              diferentes locais da cidade."
+          imageUrl="/images/mapa-sonoro.gif"
+          link="https://mapa-sonoro-de-picos-git-main-sufato1287gmailcoms-projects.vercel.app/"
+        />
       </div>
     </section>
   );
